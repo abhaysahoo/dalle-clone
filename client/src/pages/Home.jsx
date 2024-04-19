7d778f694e73648a2c868ae9e6a50307ac10f107
Abhay Sahoo <abhayshaw19999@gmail.com>
2024-04-19 19:12:42 +0530

    updating domain name to host the application on the web

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     const fetchPosts = async () => {
       try {
         setLoading(true)
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dalle-clone-zsn9.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json'
